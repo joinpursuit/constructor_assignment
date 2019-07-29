a4054b7f30a663c51fdda6d36f93977e8b7ccdcf
bsihame <sihamebazi@pursuit.org>
2019-07-29 00:11:17 -0400

    commit 3

--- constructor_assignment.js
+++ constructor_assignment.js
@@ -9,23 +9,23 @@
 //Print one of their first names.
 //
   //=============================================
-  function Family(firstName, lastName, middleName) {
-       this.firstName = firstName;
-       this.lastName = lastName;
-       this.middleName = middleName;
-  };
-  let nephew = new Family ('Faysal', 'jehhar', 'Sherif')
-  let son = new Family ('Faris', 'jehharBazi', ' ');
-  console.log(son.firstname)
+  // function Family(firstName, lastName, middleName) {
+  //      this.firstName = firstName;
+  //      this.lastName = lastName;
+  //      this.middleName = middleName;
+  // };
+  // let nephew = new Family ('Faysal', 'jehhar', 'Sherif')
+  // let son = new Family ('Faris', 'jehharBazi', ' ');
+  // console.log(son.firstname)
 // b. Write a prototype function in `Person` called
 // `fullName` that will return a formatted string
 //of an instance's full name. Call this method on
 //both the instances you created in part a.
-Family.prototype.fullName = function() {
- return `${this.firstName} ${this.lastName}`;
-}
-console.log("Son's full name:"+ " "+ son.fullName());
-console.log("Nephew's full name:"+ " " + nephew.fullName());
+// Family.prototype.fullName = function() {
+//  return `${this.firstName} ${this.lastName}`;
+// }
+// console.log("Son's full name:"+ " "+ son.fullName());
+// console.log("Nephew's full name:"+ " " + nephew.fullName());
 // =================================================
 // ## Question 2
 //
@@ -33,34 +33,79 @@ console.log("Nephew's full name:"+ " " + nephew.fullName());
 //that has properties `title`, `author` and `rating`.
 //Create some instances of `Book`.
 //
-function Book (title, author, rating) {
-  this.title = title;
-  this.author = author;
-  this.rating = rating;
-};
-let book1 = new Book("The Silent Patient", "Alex Michaelides", 9.5);
-let book2 = new Book("The Wicked King (The Folk of the Air, #2)", "Holly Black", 9);
-let book3 = new Book("Ask Again, Yes", "Mary Beth Keane", 8);
-let book4 = new Book("Dianetics: The Modern Science of Mental Health ", "L. Ron Hubbard", 5);
+// function Book (title, author, rating) {
+//   this.title = title;
+//   this.author = author;
+//   this.rating = rating;
+// };
+// let book1 = new Book("The Silent Patient", "Alex Michaelides", 9.5);
+// let book2 = new Book("The Wicked King (The Folk of the Air, #2)", "Holly Black", 9);
+// let book3 = new Book("Ask Again, Yes", "Mary Beth Keane", 8);
+// let book4 = new Book("Dianetics: The Modern Science of Mental Health ", "L. Ron Hubbard", 5);
 
 // b. Add a prototype function to `Book` called `isGood`
 //that returns `true` if its rating is
 //greater than or equal to 7
-Book.prototype.isGood = function () {
-  return this.rating >= 7;
-};
-console.log(`${book1.title} is a good book : ${book1.isGood()}`);
-console.log(`${book2.title} is a good book : ${book2.isGood()}`);
-console.log(`${book3.title} is a good book : ${book3.isGood()}`);
-console.log(`${book4.title} is a good book : ${book4.isGood()}`);
+// Book.prototype.isGood = function () {
+//   return this.rating >= 7;
+// };
+// console.log(`${book1.title} is a good book : ${book1.isGood()}`);
+// console.log(`${book2.title} is a good book : ${book2.isGood()}`);
+// console.log(`${book3.title} is a good book : ${book3.isGood()}`);
+// console.log(`${book4.title} is a good book : ${book4.isGood()}`);
+// ============================
 // ## Question 3
 //
-// a. Create a `Dog` constructor function with four properties: `name (string), breed (string), mood (string), and hungry (boolean)`.
+// a. Create a `Dog` constructor
+// function with four properties:
+// `name (string), breed (string),
+// mood (string), and hungry (boolean)`.
+function Dog(name, breed, mood, hungry) {
+  this.name = name;
+  this.breed = breed;
+  this.mood = mood;
+  this.hungry = hungry;
+}
+let dog1 = new Dog('Molly', 'Bolognese', 'Cheeful', false);
+let dog2 = new Dog('Coco', 'Chug', 'Affectionate', true);
+let dog3 = new Dog('Max', 'Cockapoo', 'playful', false);
+// console.log(dog1);
+// console.log(dog2);
+// console.log(dog3);
 //
-// b. Add a prototype function called `playFetch`. It should set the dog's `hungry` property to `true`, set its mood property to `playful`, and log "Ruff!"
+// b. Add a prototype function called `playFetch`.
+//It should set the dog's `hungry` property to `true`,
+//set its mood property to `playful`, and log "Ruff!"
+// need explanation???
+// Dog.prototype.playFetch = function() {
+//   this.hungry = true;
+//   this.mood = 'playful';
+//   console.log(this.name, ':  Ruff!');
+// }
+// dog2.playFetch();
+// dog3.playFetch();
+// console.log(dog1);
+// console.log(dog2);
+// console.log(dog3);
+// //==============================================
+// c. Add a prototype function called `feed`.
+//If the dog is hungry, it should set `hungry`
+//to `false` and print "Woof!" If the dog is not hungry,
+// it should log "The dog doesn't look hungry"
 //
-// c. Add a prototype function called `feed`. If the dog is hungry, it should set `hungry` to `false` and print "Woof!" If the dog is not hungry, it should log "The dog doesn't look hungry"
-//
+Dog.prototype.feed = function() {
+  if (this.hungry) {
+    this.hungry = false;
+    console.log(this.name, `: Woof!!`);
+  } else {
+    console.log(this.name, ` doesn't look hungry`);
+  }
+}
+dog1.feed();
+dog2.feed();
+console.log(dog1);
+console.log(dog2);
+console.log(dog3);
 // d. Add a prototype function called `toString` that returns a description of the dog:
 //
 // ## Question 4
