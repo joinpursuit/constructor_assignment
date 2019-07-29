13b66f876ef9000844ce37ef98ff0a61a2a7f94e
Suzette <suzetteislam@gmail.com>
2019-07-28 21:26:54 -0400

    completed lab

--- constructor_lab.js
+++ constructor_lab.js
@@ -144,16 +144,17 @@ function Movie(name, year, genre,cast,description){
   this.description = description;
 }
 
-let coco = new Movie('Coco', 2017, 'Adventure', ['Anthony Gonzalez', 'Gael Garcia Bernal', 'Benjamin Bratt', 'Alanna Ubach', 'Renee Victor'],  'Aspiring musician Miguel, confronts his family\'s ancestral ban on music and travels to the Land of the Dead to find his great-great-grandfather, a legendary singer.')
+let coco = new Movie('Coco', 2017, 'Adventure', ['Anthony Gonzalez', 'Gael Garcia Bernal', 'Benjamin Bratt', 'Alanna Ubach', 'Renee Victor'],  'It is about an aspiring musician Miguel, confronts his family\'s ancestral ban on music and travels to the Land of the Dead to find his great-great-grandfather, a legendary singer.')
 
 console.log(coco)
 
 // // b. Create an prototype function inside `Movie` called `blurb` that returns a formatted string describing the movie.
-// Movie.prototype
+
 Movie.prototype.blurb = function(){
-  return this.description
+  return (`${this.name} came out in ${this.year}. ${this.description} `)
 }
-console.log(coco.blurb() )
+console.log((coco.blurb()))
+
 // Ex: "Borat came out in 2006. It was an odd film starring Sacha Baron Cohen as a man named Borat who was visiting America from Kazakhstan."
 //
 //
@@ -165,13 +166,37 @@ function Vector(x,y){
   this.x = x;
   this.y = y;
 }
-//
+let v1 = new Vector(1, 2)
+let v2 = new Vector (2,3)
+let v3 = new Vector(3,4)
+console.log (v2)
 // Give the Vector prototype two methods, `plus` and `minus`, that take another vector as an argument and
 // returns a new vector that has the sum or difference of the two vectors’ (the one in `this` and the parameter) x and y values.
+Vector.prototype.plus = function (vector){
+  console.log(Vector.prototype.add)
+return  {
+  x:(this.x + vector.x), y:(this.y +vector.y)
+}
+}
+
+Vector.prototype.minus = function(newVector){
+  return {
+    x:(this.x - newVector.x), y:(this.y -newVector.y)       //new vectors needed to be put in another object
+  }
+}
+
+console.log(v1.plus(v2));
+console.log(v1.minus(v2));
+
 
 // Add a method `getLength` to the prototype that computes the length of the vector ;
 // that is, the distance of the point (x, y) from the origin (0, 0).(a^2 + b^2 = c^2)
-//
+Vector.prototype.getLength = function(){
+return Math.sqrt(this.x * this.x + this.y * this.y);
+
+}
+console.log(v3.getLength())
+
 // ```js
 // var v1 = new Vector(1, 2)
 // var v2 = new Vector(2, 3)
