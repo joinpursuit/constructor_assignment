770392e9ae0727bbc594a957b9f7a3b369eb0f6b
Bpmauge <briahanamauge@pursuit.org>
2019-07-29 18:56:27 -0400

    finish question 4 and 6

--- constructor_lab.js
+++ constructor_lab.js
@@ -26,22 +26,22 @@ Person.prototype.fullname = function () {
 };
 
 beyonce.fullname();
-solange.fullname()
+solange.fullname();
 console.log('');
 
 // ## Question 2
 // a. Create a constructor function called `Book` that has
 //properties `title`, `author` and `rating`. Create some instances of `Book`.
-console.log('----- Question 2a -----');
 function Book(title, author, rating) {
   this.title = title;
   this.author = author;
   this.rating = rating;
 };
+
 let book1 = new Book('26a', 'Diana Evans', 8.4);
 let book2 = new Book('The Coldest Winter Ever', 'Sister Souljah', 9);
 let book3 = new Book('Genesis Begins Again', 'Alicia D. Williams', 10);
-console.log('');
+
 // b. Add a prototype function to `Book` called `isGood`
 //that returns `true` if its rating is greater than or equal to 7
 console.log('----- Question 2b -----');
@@ -49,7 +49,7 @@ Book.prototype.isGood = function (rating) {
   if (this.rating >= 7) {
     return true;
   } return false;
-}
+};
 
 console.log(book1.isGood());
 console.log('');
@@ -57,7 +57,7 @@ console.log('');
 // ## Question 3
 // a. Create a `Dog` constructor function with four
 //properties: `name (string), breed (string), mood (string), and hungry (boolean)`.
-console.log('----- Question 3a -----');
+
 let dog1 = new Dog('Lucky', 'Beagle', 'happy', true);
 
 function Dog(name, breed, mood, hungry) {
@@ -65,36 +65,46 @@ function Dog(name, breed, mood, hungry) {
   this.breed = breed;
   this.mood = mood;
   this.hungry = hungry;
-}
-console.log('');
+};
+
 // b. Add a prototype function called `playFetch`.
 //It should set the dog's `hungry` property to `true`, set its mood
 //property to `playful`, and log "Ruff!"
 console.log('----- Question 3b -----');
 Dog.prototype.playFetch = function () {
-  this.hungry = true;
+  this.hungry = false;
   this.mood = 'playful';
   console.log('Ruff');
-}
-console.log(dog1.playFetch)
+};
+
+dog1.playFetch();
 console.log('');
+
 // c. Add a prototype function called `feed`.
 //If the dog is hungry, it should set `hungry` to `false` and print "Woof!"
 //If the dog is not hungry, it should log "The dog doesn't look hungry"
 console.log('----- Question 3c -----');
 Dog.prototype.feed = function () {
-  if (hungry) {
-    hungry = false;
-     return console.log('Woof');
+  if (dog1.hungry) {
+    let hungry = false;
+    return console.log('Woof');
   } else {
-     console.log('The dog doesn\'t look hungry');
+    console.log('The dog doesn\'t look hungry');
   }
 };
 
-console.log(dog1.feed)
+dog1.feed();
 console.log('');
+
 // d. Add a prototype function called `toString` that returns a description of the dog:
 console.log('----- Question 3d -----');
+Dog.prototype.toString = function () {
+  console.log(`The dog's name is ${this.name} and is a ${this.breed}. ${this.name}'s mood is ${this.mood} and it is ${this.hungry} that ${this.name} is hungry.`);
+};
+
+dog1.toString();
+console.log('');
+
 //
 // ## Question 4
 // There are three common scales that are used to measure
@@ -107,13 +117,12 @@ console.log('----- Question 3d -----');
 // a. Make an object called `freezingPoint` that has three
 //properties: `celsius`, `fahrenheit`, and `kelvin`. Give them all values
 //equal to the freezing point of water.
-console.log('----- Question 4a -----');
 let freezingPoint = {
   celsius: 0,
   fahrenheit: 32,
   kelvin: 373.15,
-}
-console.log('');
+};
+
 // b. Make a constructor function called `Celsius` that has one
 // property: `celsius`, and two methods `getFahrenheitTemp`, and `getKelvinTemp`.
 //
@@ -124,11 +133,14 @@ console.log('');
 // outsideTempt.getFahrenheitTemp() //returns 50.0
 // ```
 console.log('----- Question 4b -----');
-function Celsius () {
+function Celsius() {
 
 }
+
 console.log('');
-// c. Give `Celsius` a prototype function called `isBelowFreezing` that returns a `Bool` (true if the temperature is below freezing).
+
+// c. Give `Celsius` a prototype function called `isBelowFreezing`
+//that returns a `Bool` (true if the temperature is below freezing).
 console.log('----- Question 4c -----');
 console.log('');
 
@@ -136,34 +148,39 @@ console.log('');
 //
 // a. Create a constructor function called `Movie` that has properties
 //for `name`, `year`, `genre`, `cast`, and `description`. Create an instance of your `Movie`
-console.log('----- Question 5a -----');
+
+// b. Create an prototype function inside `Movie` called `blurb`
+//that returns a formatted string describing the movie.
+// Ex: "Borat came out in 2006. It was an odd film starring Sacha Baron
+//Cohen as a man named Borat who was visiting America from Kazakhstan."
+//
+console.log('----- Question 5b -----');
 function Movie (name, year, genre, cast, description) {
   this.name = name;
   this.year = year;
   this.genre = genre;
   this.cast = cast;
   this.description = description;
-  Movie.prototype.blurb = function () {
-  console.log(`${this.name} came out in ${this.year}. It was a ${this.genre} film starring ${cast}, as ${description}.`)
-  }
 }
-let movie1 = new Movie ('The Cheetah Girl', '2003', 'Musical',
-['Raven-Symoné', 'Adrienne Bailon', 'Kiely Williams', 'Sabrina Bryan'], 'four teens aim to take the world by storm with their music.')
-console.log(movie1.blurb);
-console.log('');
+Movie.prototype.blurb = function () {
+console.log(`${this.name} came out in ${this.year}. It was a ${this.genre} film starring ${this.cast}, as ${this.description}.`)
+}
 
-// b. Create an prototype function inside `Movie` called `blurb` that returns a formatted string describing the movie.
-// Ex: "Borat came out in 2006. It was an odd film starring Sacha Baron Cohen as a man named Borat who was visiting America from Kazakhstan."
-//
-console.log('----- Question 5b -----');
+let movie1 = new Movie ('The Cheetah Girl', '2003', 'Musical',
+['Raven-Symoné', ' Adrienne Bailon', ' Kiely Williams', ' Sabrina Bryan'], 'four teens aim to take the world by storm with their music')
+movie1.blurb();
+console.log('');
 
 // ## Question 6
 //
 // Write a constructor Vector that represents a vector in two-dimensional space.
-// It takes two number arguments: `x` and `y` parameters, which it should be saved to properties of the same name.
+// It takes two number arguments: `x` and `y` parameters,
+//which it should be saved to properties of the same name.
 //
-// Give the Vector prototype two methods, `plus` and `minus`, that take another vector as an argument and
-// returns a new vector that has the sum or difference of the two vectors’ (the one in `this` and the parameter) x and y values.
+// Give the Vector prototype two methods, `plus` and `minus`,
+//that take another vector as an argument and
+// returns a new vector that has the sum or difference of the two vectors’
+//(the one in `this` and the parameter) x and y values.
 //
 // Add a method `getLength` to the prototype that computes the length of the vector ;
 // that is, the distance of the point (x, y) from the origin (0, 0).(a^2 + b^2 = c^2)
