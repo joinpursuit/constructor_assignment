18b724c982dbe7b5f07d53ce2b3aec34b3623e73
Kevin Wong <kevinwong@pursuit.org>
2019-10-27 15:13:55 -0400

    Lab completed

--- constructor_functions.js
+++ constructor_functions.js
@@ -22,6 +22,8 @@ Person.prototype.fullName = function (){
 console.log(corey.fullName());
 console.log(kev.fullName());
 
+
+
 // ## Question 2
 
 // a. Create a constructor function called `Book` that has properties `title`, `author` and `rating`. Create some instances of `Book`.
@@ -46,6 +48,7 @@ Book.prototype.isGood = function () {
 }
 console.log(book1.isGood());
 console.log(book2.isGood());
+
 
 
 // ## Question 3
@@ -85,7 +88,12 @@ doge.feed();
 console.log(doge.hungry);
 
 // d. Add a prototype function called `toString` that returns a description of the dog:
-c
+Dog.prototype.toString = function () {
+    return "Dog's name: " + this.name + ", Dog's breed: " + this.breed + ", Dog's mood: " + this.mood;
+}
+console.log(doge.toString());
+
+
 
 // ## Question 4
 
@@ -96,69 +104,160 @@ c
 // K = C + 273
 
 // a. Make an object called `freezingPoint` that has three properties: `celsius`, `fahrenheit`, and `kelvin`. Give them all values equal to the freezing point of water.
-
+let freezingPoint = {
+    celsius: 0,
+    fahrenheit: 32,
+    kelvin: 273
+}
 
 // b. Make a constructor function called `Celsius` that has one property: `celsius`, and two methods `getFahrenheitTemp`, and `getKelvinTemp`.
+function Celsius (celsius) {
+    this.celsius = celsius;
+}
+
+Celsius.prototype.getFahrenheitTemp = function () {
+    return 1.8 * this.celsius + 32;
+}
+
+Celsius.prototype.getKelvinTemp = function () {
+    return this.celsius + 273
+}
 
 // ```js
-// let outsideTempt = new Celsius(10.0)
-// outsideTempt.celsius //returns 10.0
-// outsideTempt.getKelvinTemp() //returns 283.0
-// outsideTempt.getFahrenheitTemp() //returns 50.0
+let outsideTempt = new Celsius(-5.0)
+console.log(outsideTempt.celsius); //returns 10.0
+console.log(outsideTempt.getKelvinTemp()) //returns 283.0
+console.log(outsideTempt.getFahrenheitTemp()) //returns 50.0
 // ```
 
 // c. Give `Celsius` a prototype function called `isBelowFreezing` that returns a `Bool` (true if the temperature is below freezing).
+Celsius.prototype.isBelowFreezing = function(){
+    if(this.celsius < freezingPoint["celsius"]){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(outsideTempt.isBelowFreezing())
+
 
 // ## Question 5
 
 // a. Create a constructor function called `Movie` that has properties for `name`, `year`, `genre`, `cast`, and `description`. Create an instance of your `Movie`
+function Movie (name, year, cast, description) {
+    this.name = name;
+    this.year = year;
+    this.cast = cast;
+    this.description = description;
+}
+
+let borat = new Movie("Borat", "2006", "Sacha Boran Cohen", "It was an odd film starring Sacha Baron Cohen as a man named Borat who was visiting America from Kazakhstan.")
 
 // b. Create an prototype function inside `Movie` called `blurb` that returns a formatted string describing the movie.
+Movie.prototype.blurb = function (){
+    return this.name + " came out in " + this.year + ". " + this.description;
+}
 
 // Ex: "Borat came out in 2006. It was an odd film starring Sacha Baron Cohen as a man named Borat who was visiting America from Kazakhstan."
-
+console.log(borat.blurb());
 
 // ## Question 6
 
 // Write a constructor Vector that represents a vector in two-dimensional space.
 // It takes two number arguments: `x` and `y` parameters, which it should be saved to properties of the same name.
+function Vector (x , y) {
+    this.x = x;
+    this.y = y;
+}
 
 // Give the Vector prototype two methods, `plus` and `minus`, that take another vector as an argument and
 // returns a new vector that has the sum or difference of the two vectors’ (the one in `this` and the parameter) x and y values.
+Vector.prototype.plus = function (vector) {
+    return new Vector(this.x + vector.x, this.y + vector.y); 
+}
+
+Vector.prototype.minus = function (vector) {
+    return new Vector(this.x - vector.x, this.y - vector.y); 
+}
+
 
 // Add a method `getLength` to the prototype that computes the length of the vector ;
 // that is, the distance of the point (x, y) from the origin (0, 0).(a^2 + b^2 = c^2)
+Vector.prototype.getLength = function (){
+    let length = Math.sqrt((this.x**2 + this.y**2));
+    return length;
+}
+
 
 // [Vectors at mathisfun.com](https://www.mathsisfun.com/algebra/vectors.html)
 
 // ```js
-// var v1 = new Vector(1, 2)
-// var v2 = new Vector(2, 3)
-// console.log(v1.plus(v2));
+var v1 = new Vector(1, 2)
+var v2 = new Vector(2, 3)
+console.log(v1.plus(v2));
 // // => Vector {x: 3, y: 5}
-// console.log(v1.minus(v2));
+console.log(v1.minus(v2));
 // // => Vector {x: -1, y: -1}
 
-// var v3 = new Vector(3, 4)
-// console.log(v3.getLength());
+var v3 = new Vector(3, 4)
+console.log(v3.getLength());
 // // => 5
 // ```
 
 // ## Question 7
 
 // a. Write a constructor function called `Cylinder` that has properties `radius` and `height`.  Create an instance of a Cylinder.
+function Cylinder (radius, height) {
+    this.radius = radius;
+    this.height = height;
+}
+
+let cylinder1 = new Cylinder(2, 4);
 
 // b. Add a prototype function `getVolume` that returns the [volume](https://www.mathopenref.com/cylindervolume.html)
+Cylinder.prototype.getVolume = function () {
+    return Math.PI * this.radius**2 * this.height;
+}
+console.log(cylinder1.getVolume());
 
 // c. Add a prototype function `getSurfaceArea` that returns the [surface area](https://www.mathopenref.com/cylinderareamain.html)
+Cylinder.prototype.getSurfaceArea = function () {
+    return (2 * Math.PI * this.radius**2) + (2 * Math.PI * this.radius * this.height);
+}
+console.log(cylinder1.getSurfaceArea());
 
 // ## Question 8
 
 // [Dates in JavaScript](https://www.digitalocean.com/community/tutorials/understanding-date-and-time-in-javascript#targetText=The%20Date%20object%20is%20a,the%20current%20date%20and%20time.)
 
 // a. Write a constructor function called `Post` that has properties `datePosted`, `user`, and `text`.  Create an array of `Post` objects.
+function Post (datePosted, user, text) {
+    this.datePosted = datePosted;
+    this.user = user;
+    this.text = text;
+} 
+
+let postObjs = [
+    new Post(new Date(2019,10,27), "corey", "Who's ready for some JavaScript"),
+    new Post(new Date(2019,10,26), "kelvin", "Idk man"),
+    new Post(new Date(2019,10,28), "kevin", "yerrrrr")
+]
 
 // b. Create a prototype function that returns whether or not the post was made today.
+Post.prototype.isPostedToday = function() {
+    let today = new Date();
+    if (today.getDate() === this.datePosted.getDate()){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log("Is this post today? " + postObjs[0].isPostedToday());
+console.log("Is this post today? " + postObjs[1].isPostedToday());
 
 // c. Filter your array of `Post` objects to only include posts made today.
-
+console.log(postObjs.filter((el) => {
+    if(el.isPostedToday()){
+        return el;
+    }
+}))
