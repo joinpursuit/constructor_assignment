4d87fb87647824e4ecabb17953ddb3db7baa63a9
Cassidy Beni <cassidybeni@pursuit.org>
2019-10-27 12:32:13 -0400

    complete question 2

--- constructor_lab.js
+++ constructor_lab.js
@@ -18,3 +18,26 @@ Person.prototype.fullName = function(){
 }
 console.log(jay.fullName())
 
+// ## Question 2
+
+// a. Create a constructor function called `Book` that has properties `title`, `author` and `rating`. Create some instances of `Book`.
+
+function Book(title, author, rating) {
+    this.title = title
+    this.author = author
+    this.rating = rating
+}
+let precious = new Book("Push", "Sapphire", 8)
+let daisy = new Book("TheGreatGatsby", "F. Scott Fitzgerald", 6)
+let lenny = new Book("Of Mice and Men", "John Steinbeck", 10)
+
+// b. Add a prototype function to `Book` called `isGood` that returns `true` if its rating is greater than or equal to 7
+
+Book.prototype.isGood = function(){
+    if(this.rating >= 7){
+        return true
+    } else {
+        return "It's not a good book"
+    }
+}
+console.log(precious.isGood())
