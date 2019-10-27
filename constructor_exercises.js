102e84735c788d27d4e468936805021d59fdc83f
Samantha Jimenez <samanthajimenez@pursuit.org>
2019-10-27 12:44:21 -0400

    complete problem 3

--- constructor_exercises.js
+++ constructor_exercises.js
@@ -1,35 +1,70 @@
-//Q1a
+// //Q1
 
-function Person(firstName, lastName, middleName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.middleName = middleName;
+// function Person(firstName, lastName, middleName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.middleName = middleName;
+// }
+
+// let sam = new Person("Samantha", "Jimenez", "B")
+// let erik = new Person("Erikson", "Castro", "A")
+
+// // console.log(erik.firstName);
+
+// Person.prototype.fullName = function() {console.log(this.firstName + " " + this.middleName + " " + this.lastName)}
+
+// erik.fullName();
+// sam.fullName();
+
+// //Q2
+
+// function Book(title, author, rating){
+//     this.title= title;
+//     this.author= author;
+//     this.rating= rating;
+// }
+// let book1=new Book("Phaedo", "Plato", 10);
+// let book2= new Book("Harry Potter", "J.K. Rowling", 6);
+// Book.prototype.isGood= function () {
+//     if(this.rating>=7){
+//     console.log("True");   
+// }
+// }
+// book1.isGood();
+
+//Q3.
+
+function Dog(name, breed, mood, hungry) {
+    this.name = name;
+    this.breed = breed;
+    this.mood = mood; 
+    this.hungry = hungry;
+}
+Dog.prototype.playFetch = function() {
+    this.hungry = true;
+    this.mood = "playful";
+    console.log("Ruff!");
 }
 
-let sam = new Person("Samantha", "Jimenez", "B")
-let erik = new Person("Erikson", "Castro", "A")
+let spot = new Dog("spot", "boxer", "angry", true);
+// spot.playFetch();
+// console.log(spot);
 
-// console.log(erik.firstName);
-
-//Q1b
-
-Person.prototype.fullName = function() {console.log(this.firstName + " " + this.middleName + " " + this.lastName)}
-
-erik.fullName();
-sam.fullName();
-
-//Q2a
-
-function Book(title, author, rating){
-    this.title= title;
-    this.author= author;
-    this.rating= rating;
+Dog.prototype.feed = function() {
+    if(this.hungry === true) {
+        hungry = false
+        console.log("Woof!")
+    } else {
+        console.log("The dog doesn't look hungry")
+    }
 }
-let book1=new Book("Phaedo", "Plato", 10);
-let book2= new Book("Harry Potter", "J.K. Rowling", 6);
-Book.prototype.isGood= function () {
-    if(this.rating>=7){
-    console.log("True");   
+// spot.feed();
+// console.log(spot);
+
+Dog.prototype.toString = function() {
+    console.log(`${this.name} is a ${this.mood} ${this.breed}`);
 }
-}
-book1.isGood();
+// spot.toString();
+// console.log(spot);
+
+//Q4
