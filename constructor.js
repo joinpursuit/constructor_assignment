f6c56b261abc413211bde01fed5fb0eb7ba17578
John Jones Jr <jayjohnjones@gmail.com>
2019-10-27 15:10:35 -0400

    Question 6

--- constructor.js
+++ constructor.js
@@ -171,3 +171,46 @@ console.log(transformers.blurb())
 //
 
 // Ex: "Borat came out in 2006. It was an odd film starring Sacha Baron Cohen as a man named Borat who was visiting America from Kazakhstan."
+
+// ## Question 6
+ 
+// Write a constructor Vector that represents a vector in two-dimensional space.
+// It takes two number arguments: `x` and `y` parameters, which it should be saved to properties of the same name.
+
+// Give the Vector prototype two methods, `plus` and `minus`, that take another vector as an argument and
+// returns a new vector that has the sum or difference of the two vectors’ (the one in `this` and the parameter) x and y values.
+
+// Add a method `getLength` to the prototype that computes the length of the vector ;
+// that is, the distance of the point (x, y) from the origin (0, 0).(a^2 + b^2 = c^2)
+
+// [Vectors at mathisfun.com](https://www.mathsisfun.com/algebra/vectors.html)
+
+function Vector (x,y){
+    this.x = x
+    this.y = y;
+}
+
+Vector.prototype.plus = function(vector){
+    let sumX = this.x + vector.x
+    let sumY = this.y + vector.y
+    let newVector = new Vector(sumX, sumY)
+    return newVector
+}
+
+Vector.prototype.minus = function(vector){
+    let minusX = this.x - vector.x
+    let minusY = this.y - vector.y
+    let newVector = new Vector(minusX, minusY);
+    return newVector;
+}
+
+Vector.prototype.getLength = function(){
+    return(Math.sqrt((this.x * this.x) + (this.y * this.y)));
+}
+
+let vec1 = new Vector(1,2);
+let vec2 = new Vector(2,3);
+console.log(vec1.minus(vec2))
+
+let vec3 = new Vector(3,4)
+console.log(vec3.getLength())
