a8fde6f54bb1bbfa243fe902cfe5c260efc14bfb
Brandon Brown <brandonb0206@gmail.com>
2019-10-27 12:59:56 -0400

    question 3 complete

--- constructor.js
+++ constructor.js
@@ -36,3 +36,35 @@ let book3 = new Book("The Great Gatsby", "F.S Fitzgerald", 10)
 console.log(book1.isGood())
 
 
+console.log("Question 3")
+
+function Dog(name, breed, mood, hungry) {
+    this.name = name
+    this.breed = breed
+    this.mood = mood
+    this.hungry = hungry
+}
+Dog.prototype.playFetch = function() {
+    this.hungry = true
+    this.mood = "playful "
+    console.log("Ruff!")
+}
+Dog.prototype.feed = function () {
+    if(this.hungry === true){
+        this.hungry = false
+        console.log("Woof!")
+    }else {
+        console.log("The dog doesn't look hungry")
+        
+    }
+}
+Dog.prototype.toString = function(){
+    console.log(`${this.name}${this.breed}${this.mood}${this.hungry}`)
+}
+let dog1 = new Dog("Bingo ", "Husky ", "Happy ", false)
+dog1.playFetch()
+dog1.feed()
+dog1.toString()
+
+
+
