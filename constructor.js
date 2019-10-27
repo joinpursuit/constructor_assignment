cca54db6617bd9ca1aa909ec544ff16788aa28bb
Brandon Brown <brandonb0206@gmail.com>
2019-10-27 12:33:02 -0400

    Completed question 2

--- constructor.js
+++ constructor.js
@@ -13,3 +13,26 @@ let person2 = new Person("Deja ", "flynn ", "Lauren ")
 // console.log(person1)
 person1.fullName()
 
+console.log("Question 2")
+
+function Book(title, author, rating) {
+    this.title = title
+    this.author = author
+    this.rating = rating
+
+}
+Book.prototype.isGood = function(){
+     if(this.rating >= 7) {
+         return true
+     }else {
+         return false
+     }
+}
+
+let book1 = new Book("Ulysses", "James Joyce", 5)
+let book2 = new Book("Great Expectations", "Charles Dickens", 8)
+let book3 = new Book("The Great Gatsby", "F.S Fitzgerald", 10)
+
+console.log(book1.isGood())
+
+
