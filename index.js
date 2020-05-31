ce6537f5a15cae760fffcca0834d2ebe7e0d583f
cassidybeni <cassidybeni@pursuit.org>
2020-05-31 18:20:38 -0400

    Complete problem 2a

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
-//Question 1a
+// //Question 1a
 
-function Person (firstName, lastName, middleName) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.middleName = middleName
-}
+// function Person (firstName, lastName, middleName) {
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.middleName = middleName
+// }
   
-let cassidy = new Person("Cassidy", "Beni", "E")
-let john = new Person("John", "Doe", "Michael")
+// let cassidy = new Person("Cassidy", "Beni", "E")
+// let john = new Person("John", "Doe", "Michael")
   
-console.log(cassidy["firstName"])
+// console.log(cassidy["firstName"])
 
-//Question 1b
+// //Question 1b
 
-Person.prototype.fullName = function() {
-    return `${this.firstName} ${this.middleName} ${this.lastName}`
+// Person.prototype.fullName = function() {
+//     return `${this.firstName} ${this.middleName} ${this.lastName}`
+// }
+
+// console.log(cassidy.fullName())
+
+// //Question 2
+
+function Book (title, author, rating) {
+    this.title = title
+    this.author = author
+    this.rating = rating
 }
 
-console.log(cassidy.fullName())
+let firstBook = new Book ("Of Mice and Men", "John Steinbeck", 10)
+let secondBook = new Book ("Insomnia", "Stephen King", 10)
 
+Book.prototype.isGood = function() {
+    if (this.rating >= 7) {
+        return true
+    } else {
+        return false
+    }
+}
 
+console.log(firstBook.isGood())
